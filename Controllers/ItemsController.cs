1a645d167e675a9b1adbd976b4fc05e12fc96291
nightSanches <galesavich@gmail.com>
2024-05-24 20:13:17 +0500

    Добавьте файлы проекта.

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using pr37savichev.Data.Interfaces;
+using pr37savichev.Data.Models;
+using pr37savichev.Data.ViewModell;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace pr37savichev.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private IItems IAllItems;
+        private ICategories IAllCategories;
+        VMItems VMItems = new VMItems();
+
+        public ItemsController(IItems IAllItems, ICategories IAllCategories)
+        {
+            this.IAllItems = IAllItems;
+            this.IAllCategories = IAllCategories;
+        }
+
+        public ViewResult List(int id = 0)
+        {
+            ViewBag.Title = "Страница с предметами";
+            VMItems.Items = IAllItems.AllItems;
+            VMItems.Categories = IAllCategories.AllCategories;
+            VMItems.SelectCategory = id;
+            return View(VMItems);
+        }
+
+        
+    }
+}
---
+++ Data/DataBase/DBItems.cs
@@ -0,0 +1,38 @@
+﻿using MySql.Data.MySqlClient;
+using pr37savichev.Data.Interfaces;
+using pr37savichev.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace pr37savichev.Data.DataBase
+{
+    public class DBItems : IItems
+    {
+        public IEnumerable<Categories> Categories = new DBCategory().AllCategories;
+
+        public IEnumerable<Items> AllItems
+        {
+            get
+            {
+                List<Items> items = new List<Items>();
+                MySqlConnection mySqlConnection = Common.Connection.MySqlOpen();
+                MySqlDataReader ItemsData = Common.Connection.MySqlQuery($"SELECT * FROM Shop.items ORDER BY `Name`;", mySqlConnection);
+                while (ItemsData.Read())
+                {
+                    items.Add(new Items()
+                    {
+                        Id = ItemsData.IsDBNull(0) ? -1 : ItemsData.GetInt32(0),
+                        Name = ItemsData.IsDBNull(1) ? "" : ItemsData.GetString(1),
+                        Description = ItemsData.IsDBNull(2) ? "" : ItemsData.GetString(2),
+                        Img = ItemsData.IsDBNull(3) ? "" : ItemsData.GetString(3),
+                        Price = ItemsData.IsDBNull(4) ? -1 : ItemsData.GetInt32(4),
+                        Category = ItemsData.IsDBNull(5) ? null : Categories.Where(x => x.Id == ItemsData.GetInt32(5)).First()
+                    });
+                }
+                mySqlConnection.Close();
+
+                return items;
+            }
+        }
+    }
+}
